87dead94102ff9c0bcadf6c902ccf9058a6db4b4
sheldwu <wxd@dev72.localdomain>
2014-02-20 17:55:40 +0800

    delete

--- util/t.cpp
+++
@@ -1,9 +0,0 @@
-#include "my_func.h"
-#include "my_redis.h"
-
-int main(int argc, char** argv){
-    my_redis* ptr_rd = new my_redis("10.101.0.72:6379;10.101.0.72:6380;10.101.0.72:6381");
-    ptr_rd->connect();
-    ptr_rd->select(4);
-    ptr_rd->
-}
